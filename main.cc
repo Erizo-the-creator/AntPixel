c71d45f30b648bc2f95b202f539351b8d3c79704
Erizo <WhiteShadow4007@gmail.com>
2021-11-06 19:31:06 +0100

    init

--- main.cc
+++ main.cc
@@ -1,17 +1,16 @@
 #include <SDL2/SDL.h>
 #include <stdlib.h>
-#include <ctime>
+#include <time.h>
 #define GRID_CELL_SIZE 36
 #define GRID_WIDTH 29
 #define GRID_HEIGHT 23
 #define MOVE_SPEED 0.01f
 #define UPDATES_PER_SECOND 100
-int main()
-{
+#undef main
+int main(int argc, char* argv[]) {
     srand(time(NULL));
     int window_width = (GRID_WIDTH * GRID_CELL_SIZE) + 1;
     int window_height = (GRID_HEIGHT * GRID_CELL_SIZE) + 1;
-    int counter=0;
 
     SDL_Rect antInstance = {
         (GRID_WIDTH - 1) / 2 * GRID_CELL_SIZE,
@@ -31,16 +30,10 @@ int main()
     }
 
     //SDL_Window *window;
-    SDL_Renderer *renderer;
     
-    SDL_Window *window = SDL_CreateWindow("AntPixel", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height,SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow("AntPixel", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, window_width, window_height,SDL_WINDOW_OPENGL);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     
-    SDL_CreateWindowAndRenderer(window_width, window_height, SDL_WINDOW_OPENGL, &window, &renderer);
-    
-    if(SDL_CreateWindowAndRenderer(window_width, window_height, SDL_WINDOW_OPENGL , &window, &renderer) < 0){
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Failed to initialize window and renderer: %s", SDL_GetError());
-        return -1;
-    }
 
     SDL_SetWindowTitle(window, "AntPixel");
     SDL_bool quit = SDL_FALSE;
@@ -57,8 +50,6 @@ int main()
         }
         SDL_SetRenderDrawColor(renderer, grid_background.r, grid_background.g, grid_background.b, grid_background.a);
         
-        //SDL_RenderClear(renderer); 
-        
         SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g, grid_line_color.b, grid_line_color.a);
 
             for (int x = 0; x < 1 + GRID_WIDTH * GRID_CELL_SIZE; x += GRID_CELL_SIZE)
@@ -74,40 +65,11 @@ int main()
             SDL_RenderFillRect(renderer, &antInstance);
             SDL_RenderPresent(renderer);
             
-            if(counter % 100 = 0){
-
-                int rotation = rand() % 4;
-            }
-            
-            move(MOVE_SPEED,rotation);
-
+        
+    }
             SDL_DestroyRenderer(renderer);
             SDL_DestroyWindow(window);
             SDL_Quit();
 
         return 0;
-    }
-}
-void move(float speed,enum ROTATION){
-    enum ROTATION{
-        LEFT = 1,
-        RIGHT = 2,
-        UP = 3,
-        DOWN = 4,
-    };
-    
-    switch(ROTATION{}){
-        
-        case RIGHT:
-            
-            break;
-        case LEFT:
-
-            break;
-        case UP:
-
-            break;
-        case DOWN:
-
-            break;
 }
--- makefile
+++ makefile
@@ -1,10 +1,9 @@
-LIBS = -L lib -l SDL2main -l mingw32 -l SDL2 
-
-FLAGS = -mwindows 
+LIBS = -L lib  -l SDL2main -l SDL2 
 
 all: compile link
 
 compile:
-	g++ -I include -c *.cc $(FLAGS)
+	g++ -I include -c *.cc 
+
 link:
 	g++ main.o -o bin/AntPixel.exe $(LIBS)	
