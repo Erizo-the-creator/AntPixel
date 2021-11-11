46be4731164b61dbc70c9708eedd5e206884019d
Rotthin <rotthin_dev@protonmail.com>
2021-11-11 20:33:03 +0100

    Grid resizes with window

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,17 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>	
 #include <stdio.h>
 
-#define GRID_WIDTH 900
-#define GRID_HEIGHT 900
-
 #define MOVE_SPEED 0.01f
 #undef main
 
 #define RGBA(col) col.r, col.b, col.b, col.a
+
+#define DEF_WND_W 900
+#define DEF_WND_H 900
 
 static const SDL_Color ANT_COLOR = {255,255,255,255};
 static const SDL_Color BG_COLOR = {27, 33, 70, 255};
@@ -18,7 +19,9 @@ static const SDL_Color GRID_COLOR = {13, 12, 50, 255};
 
 static SDL_Window *window;
 static SDL_Renderer *renderer;
+
 static int cell_size;
+static int wnd_w, wnd_h;
 
 static SDL_Rect ant_rect;
 
@@ -31,12 +34,12 @@ static void draw_bg(void) {
 static void draw_grid(void) {
         SDL_SetRenderDrawColor(renderer, RGBA(GRID_COLOR));
 
-        for(unsigned int x = 0; x < GRID_WIDTH; x += cell_size) {
-                SDL_RenderDrawLine(renderer, x, 0, x, GRID_HEIGHT);
+        for(unsigned int x = 0; x < wnd_w; x += cell_size) {
+                SDL_RenderDrawLine(renderer, x, 0, x, wnd_h);
         }
 
-        for(unsigned int y = 0; y < GRID_HEIGHT; y += cell_size) {
-                SDL_RenderDrawLine(renderer, 0, y, GRID_WIDTH, y);
+        for(unsigned int y = 0; y < wnd_h; y += cell_size) {
+                SDL_RenderDrawLine(renderer, 0, y, wnd_w, y);
         }
 }
 
@@ -56,12 +59,15 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	window = SDL_CreateWindow("AntPixel", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900, 900, SDL_WINDOW_SHOWN);
+	window = SDL_CreateWindow("AntPixel", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, DEF_WND_W, DEF_WND_H, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
+        wnd_w = DEF_WND_W;
+        wnd_h = DEF_WND_H;
+
 	ant_rect = {
-		(GRID_WIDTH + 1) / 2,
-		(GRID_HEIGHT + 1) / 2,
+		(DEF_WND_W + 1) / 2,
+		(DEF_WND_H + 1) / 2,
 		cell_size,
 		cell_size,
 	};
@@ -80,6 +86,14 @@ int main(int argc, char* argv[]) {
 				case SDL_QUIT:
 					quit = true;
 					break;
+
+                                case SDL_WINDOWEVENT:
+                                        switch(ev.window.event) {
+                                                case SDL_WINDOWEVENT_RESIZED:
+                                                        wnd_w = ev.window.data1;
+                                                        wnd_h = ev.window.data2;
+                                                        break;
+                                        }
 			}
 		}
 
